ac4da89c21fc93bf54f6497a9f43ebc8243ee45a
unknown <james.c.noonan@gmail.com>
2019-01-28 19:32:40 -0500

    init

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+ credentials = "${file("k8s-node-224301-737b6d94efa1.json")}"
+ project     = "k8s-node-224301"
+ region      = "us-east1"
+}
+
+resource "google_container_cluster" "gcp_kubernetes" {
+    name               = "${var.cluster_name}"
+    zone               = "us-east1-b"
+    initial_node_count = 1
+    additional_zones = [
+        "us-east1-c",
+        "us-east1-d",
+    ]
+    master_auth {
+        username = "${var.linux_admin_username}"
+        password = "${var.linux_admin_password}}"
+    }
+    node_config {
+        oauth_scopes = [
+          "https://www.googleapis.com/auth/compute",
+          "https://www.googleapis.com/auth/devstorage.read_only",
+          "https://www.googleapis.com/auth/logging.write",
+          "https://www.googleapis.com/auth/monitoring",
+        ]
+        labels {
+            this-is-for = "dev-cluster"
+        }
+        tags = ["dev", "work"]
+    }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+cluster_name = "gcpk8sjames"
+gcp_cluster_count = 1
+linux_admin_username = "james"
+linux_admin_password = "supersecretpassword"
